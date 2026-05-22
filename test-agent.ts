fd18ed1e62dcaa9a64fc0f67bbcae33275369d1f
sneshinde <snehalshinde2112@gmail.com>
2026-05-22 14:11:19 +0200

    cleanup

--- test-agent.ts
+++
@@ -1,8 +0,0 @@
-import "dotenv/config";
-import { runAgent } from "./src/agent/index.js";
-
-const prompt = process.argv[2] ?? "draw a simple login flow";
-console.log(`User: ${prompt}\n`);
-
-const response = await runAgent(prompt);
-console.log(`\nAgent: ${response}`);
